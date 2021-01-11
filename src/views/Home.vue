4fb2a97bbfcb0ef28dd7d9bb3a356f15d6bbe669
Arthur Oliveira <arthurolvmorais@gmail.com>
2021-01-11 09:09:51 -0300

    feat(amplify-google-auth): implements google authentication

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,69 @@
 <template>
   <div class="home">
     <h1>Amplify Auth</h1>
+
+    <template v-if="isLogged">
+      <p>
+        Logged as <strong>{{ user.email }}</strong>
+      </p>
+
+      <button @click="logout()">
+        LOGOUT
+      </button>
+    </template>
+
+    <button @click="login()" v-else>
+      LOGIN WITH GOOGLE
+    </button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { Auth } from 'aws-amplify';
+import { onMounted, reactive, computed, toRefs } from 'vue';
+import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth/lib-esm/types';
 
-export default defineComponent({
+function login() {
+  Auth.federatedSignIn({
+    provider: CognitoHostedUIIdentityProvider.Google,
+  });
+}
+
+function checkUser() {
+  return Auth.currentAuthenticatedUser()
+    .then(user => user)
+    .catch(() => ({}));
+}
+
+export default {
   name: 'Home',
-});
+
+  setup() {
+    const state = reactive({
+      user: {},
+    });
+
+    const isLogged = computed(() => !!Object.keys(state.user).length);
+
+    onMounted(async () => {
+      const user = await checkUser();
+      state.user = user?.attributes || {};
+    });
+
+    const logout = () => {
+      Auth.signOut().catch(() => {
+        state.user = {};
+      });
+    };
+
+    return {
+      login,
+      logout,
+      isLogged,
+      ...toRefs(state),
+    };
+  },
+};
 </script>
 
 <style lang="scss">
